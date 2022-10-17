d1787219256e3fad8d4e3218c6f1876e06e9c7f0
rbverissimo <rverissimo.dev@gmail.com>
2022-10-17 11:35:38 -0300

    using comprehension instead of for loops example

--- list-dictionary-comprehension-basics/dic-comprehension.py
+++ list-dictionary-comprehension-basics/dic-comprehension.py
@@ -15,3 +15,12 @@ print("\nDoubled keys iterating over dict1", double_keys)
 
 # note that we are storing a new dictionary based on an existing one
 
+numbers = range(10)
+new_dict = {}
+for n in numbers:
+    if n % 2 == 0:
+        new_dict[n] = n ** 2  # the index will receive the double power of its value
+
+
+# the same result could be achieved by implementing the following
+new_dict_comp = {n: n ** 2 for n in numbers if n % 2 == 0}  # much simpler to read
