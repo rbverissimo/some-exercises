9f312ebff414586f8f1d9b4e60c2d6fc0cddd81f
rbverissimo <rverissimo.dev@gmail.com>
2022-10-19 12:08:41 -0300

    orgnazing file and adding some conditionals knowledge

--- list-dictionary-comprehension-basics/dic-comprehension.py
+++ list-dictionary-comprehension-basics/dic-comprehension.py
@@ -3,15 +3,16 @@
 
 dict1 = {'a': 1, 'b': 2, 'c': 3, 'd': 4}
 
-print(dict1.keys())  # it will print all keys
-print(dict1.values())
-print(dict1.items())  # both keys and values
+print("\nFirst some basics in dictionary operations:")
+print("\nPrinting just the keys using the .keys() method: ", dict1.keys())  # it will print all keys
+print("Printing just the values based on values() method: ", dict1.values())
+print("The items() method returns both keys and values: ", dict1.items())  # both keys and values
 
 double_dict1 = {k: v*2 for (k, v) in dict1.items()}  # iterate over key and value and double value in dict1
 print("\nDict1 values doubled: ", double_dict1.values())
 
 double_keys = {k*2: v for (k, v) in dict1.items()}
-print("\nDoubled keys iterating over dict1", double_keys)
+print("Doubled keys iterating over dict1:", double_keys)
 
 # note that we are storing a new dictionary based on an existing one
 
@@ -33,11 +34,13 @@ f = {'t1': -30,
 
 # creating a list with the values provided by the f dictionary
 celsius = list(map(lambda x: (float(5)/9) * (x-32), f.values()))
+print("\nLet's print the list created into the celsius variable: ", celsius)
+print("Remember: this list was created using a lambda function")
 # float returns a float provided by number or string, if nothing is passed it fucking returns 0.0
 
 # returning a dict that combines each element of the dict to the celsius list
-c_dict = dict(zip(f.keys(), celsius))
-print(c_dict)
+c_dict = dict(zip(f.keys(), celsius)) # the zip() method will combine the keys with a given list
+print("\nThe new dict bred from the zip() method into: ", c_dict)
 
 # the same solution for the same problem using Dict Comp
 
@@ -45,5 +48,20 @@ f_comp = {'t1': 0, 't2': 100}
 # iterate over two variables k, v, put keeping k as is and modifying v using the formula
 c_comp = {k: (float(5)/9) * (v - 32) for (k, v) in f_comp.items()}
 print("\nUsing dict comprehension: \n", c_comp)
+
+# conditionals
+# i will use dict1 from the beginning of this file for example
+dict2 = {k: v for(k, v) in dict1.items() if v > 1}  # create a new dict with 2, 3, 4
+print("\nUsing conditionals to create this dict: ", dict2)
+
+# multiple ifs
+dict3 = {k: v for (k, v) in dict1.items() if v > 2 if v % 2 == 0}
+print("\nA new dict was created using double conditional: ", dict3)
+
+#if-else
+
+
+
+
 
 
