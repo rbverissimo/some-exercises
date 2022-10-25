a92ebc726e7712adf9f1ac98de9b87b57e9258ea
rbverissimo <rverissimo.dev@gmail.com>
2022-10-25 12:00:17 -0300

    some ideas onto how to pass super() and inheritance of methods and attributes

--- oop/inheritance-encapsulation.py
+++ oop/inheritance-encapsulation.py
@@ -6,11 +6,34 @@ class Guitar:
     def play(self):
         if self.n_strings == 6:
             print("6-string melody playing")
+        elif self.n_strings > 6:
+            print("loud melody rocking".upper())
         else:
             print("Non 6-string melody")
+
+    def details(self):
+        print(f'This is a {self.n_strings} guitar')
+
+
+class ElectricGuitar(Guitar):
+    def __init__(self):
+        self.n_strings = 8
+        self.play()  # using this method without taking it from the super()
+
+
+class BassGuitar(Guitar):
+    def __init__(self):
+        super().__init__()  # it will first initialize attributes and methods biased by that parent class
+        self.n_strings = 4
 
 
 if __name__ == '__main__':
     my_guitar = Guitar()
     print("There are", my_guitar.n_strings, "strings on this guitar")
     # my_guitar.play()
+    print("\nJust got my bass guitar: ")
+    my_bass = BassGuitar()
+    my_bass.details()
+
+    print("\nJust got an 8-stringed guitar:")
+    my_8_strings = ElectricGuitar()
